59252667e8f1637ae07bd2c84db8550c1afb0ae6
MNGOYT <mangogaming143@gmail.com>
2026-06-15 15:20:37 +1000

    server.js

--- server.js
+++ server.js
@@ -9,19 +9,38 @@ const wss = new WebSocket.Server({ server });
 
 let clients = [];
 
-// Serve frontend
 app.use(express.static(path.join(__dirname, "public")));
 
 wss.on("connection", (ws) => {
+  ws.username = "Anonymous";
   clients.push(ws);
 
   ws.on("message", (message) => {
-    // Broadcast message to everyone
-    clients.forEach(client => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message.toString());
+    try {
+      const data = JSON.parse(message);
+
+      // Set username
+      if (data.type === "login") {
+        ws.username = data.username;
+        return;
       }
-    });
+
+      // Chat message
+      if (data.type === "message") {
+        const fullMessage = JSON.stringify({
+          username: ws.username,
+          text: data.text
+        });
+
+        clients.forEach(client => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(fullMessage);
+          }
+        });
+      }
+    } catch (err) {
+      console.log("Error:", err);
+    }
   });
 
   ws.on("close", () => {
@@ -31,5 +50,5 @@ wss.on("connection", (ws) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log("Server running...");
 });
